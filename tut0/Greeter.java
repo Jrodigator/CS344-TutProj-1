aca679ce5bd6fde97482b2665256ddfe20c8914d
Rodrigues, J, Mr [25983792@sun.ac.za] <25983792@sun.ac.za>
2023-08-02 16:13:06 +0200

    [16#] I dont know what java doc comments are :(

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,33 +1,9 @@
-<<<<<<< HEAD
-public class MyClass {
-
-    public static void main(String[] args) {
-        // Example usage
-        String inputString = "Hello, world!";
-        String resultString = getName(inputString);
-        
-        System.out.println("Result string: " + resultString);
-    }
-
-    public static String getName(String x) {
-        return x;
-=======
 public class Main {
-    public static void main(String[] args) {
-        if (args.length > 0) {
-            String name = args[0];
-            greeter(name);
-        } else {
-            System.out.println("Please provide a name as a command line argument..");
-        }
-    }
 
-    public static void greeter(String name) {
-        System.out.println("Hello, " + name + "!");
-        System.out.println("Welcome to the program.");
-        System.out.println("We are glad you are here.");
-        System.out.println("This program demonstrates the use of command line arguments.");
-        System.out.println("Happy coding, " + name + "!");
->>>>>>> feature_argument_greeter
+    public static void main(String[] args) {
+        goodbye();
+    }
+    public static void goodbye() {
+        System.out.println("Goodbye! Thank you for using our program.");
     }
 }
