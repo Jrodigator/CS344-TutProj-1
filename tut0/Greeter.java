75d3eb6de8eb6b67284705c44de48bf5f03c288e
Rodrigues, J, Mr [25983792@sun.ac.za] <25983792@sun.ac.za>
2023-08-02 15:54:10 +0200

    [#9] -amended- Something about me starting my own univerity, because clearly im a genius!-amended-

---
+++ tut0/Greeter.java
@@ -0,0 +1,14 @@
+public class MyClass {
+
+    public static void main(String[] args) {
+        // Example usage
+        String inputString = "Hello, world!";
+        String resultString = getName(inputString);
+        
+        System.out.println("Result string: " + resultString);
+    }
+
+    public static String getName(String x) {
+        return x;
+    }
+}
