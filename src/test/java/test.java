5790face731232702cb999d14cdf23221dbea41a
Rodrigues, J, Mr [25983792@sun.ac.za] <25983792@sun.ac.za>
2023-08-03 12:58:57 +0200

    [#1] - Gradle Project Set Up - basic main() test, basic script.

---
+++ src/test/java/test.java
@@ -0,0 +1,14 @@
+// 25983792 SU Jared Rodrigues
+// The test class for the project
+package scripts;
+
+import org.junit.Test;
+
+public class test {
+
+    @Test
+    public void main() {
+        // Test code here
+        scripts.script.main(null);
+    }
+}
